265f28ba93dab0cfef721c1351be8d2e60429333
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-08-09 16:31:57 +0200

    chore: Add Message model and chat functionality to index page

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
-# Create your models here.
+class Message(models.Model):
+    test = models.CharField(max_length=500)
+    created_ad = models.DateField(default=date.today)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+
