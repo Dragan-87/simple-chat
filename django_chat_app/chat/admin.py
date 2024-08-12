e439e972133175f7d1b4675b7cd55a483ec58c86
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-08-12 13:52:36 +0200

    Refactor migrations and add initial Message model

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
+
+""" class MessageAdmin(admin.ModelAdmin):
+    fields = ('test', 'created_at', 'author', 'receiver')
+    list_display = ('test', 'created_at', 'author', 'receiver')
+ """
+
 
 # Register your models here.
+admin.site.register(Message)
--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -3,8 +3,8 @@ from datetime import date
 from django.conf import settings
 
 class Message(models.Model):
-    test = models.CharField(max_length=500)
-    created_ad = models.DateField(default=date.today)
+    text = models.CharField(max_length=500)
+    created_at = models.DateField(default=date.today)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
 """     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)      """
