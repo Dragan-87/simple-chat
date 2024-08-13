d0dcf01f54417f7d2ef9f8d49ac5a3ae7a5f6320
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-08-13 14:16:48 +0200

    chore: Update form submission in chat view and add message sending functionality

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from .models import Message, Chat
 from django.contrib.auth.decorators import login_required
-
+from django.http import JsonResponse
 
 @login_required(login_url='/login/')
 def index(request):
-    if request.method == "POST" and request.POST['textmessage'] != '' :
-        print("received data " + request.POST['textmessage'])
+    if request.method == "POST" and request.POST['textmessage'] != '':
         myChat = Chat.objects.get(id=1)
         Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        latest_msg = Message.objects.latest('id')
+    
+        response_data = {
+            'success': True,
+            'message': 'Nachricht erfolgreich gesendet.',
+            'messageText': latest_msg.text,
+            'messageAuthor': latest_msg.author,
+            'messageCreate': latest_msg.created_at,
+        }
+        return JsonResponse(response_data)
+
 
     chatMessages = Message.objects.filter(chat__id=1)
-    return render(request, 'chat/index.html', {'messeges': chatMessages})
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
--- django_chat_app/static/js/script.js
+++ django_chat_app/static/js/script.js
@@ -16,3 +16,49 @@ function checkPasswordMatch() {
   document.getElementById('registerButton').disabled = false;
   return true;
 }
+
+function isMessageTextEmpty() {
+  const chatMsg = document.getElementById('chatMsg').value;
+  if (chatMsg === '') {
+    document.getElementById('sendButton').disabled = true;
+    return false;
+  }
+  document.getElementById('sendButton').disabled = false;
+  return true;
+}
+
+function setFormData() {
+  let form = new FormData();
+  let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+  form.append('textmessage', chatMsg.value);
+  form.append('csrfmiddlewaretoken', token);
+  return form;
+}
+
+async function sendMessage() {
+  let form = setFormData();
+  let requestOptions = {
+    method: 'POST',
+    body: form,
+  };
+  msgContainer.innerHTML += ` <div id="deleteMsg"><span class="mr-8 gray">${new Date().toLocaleDateString(
+    'en-US',
+    { month: 'short', day: 'numeric', year: 'numeric' }
+  )}</span> <b>${username.value}</b>: <span class="gray">${
+    chatMsg.value
+  }</span></div>`;
+  try {
+    const request = await fetch('/chat/', requestOptions);
+    if (!request.ok) {
+      // Log the response text if the request was not successful
+      const errorText = await request.text();
+      console.error('Server Error:', errorText);
+      throw new Error('Server error');
+    }
+    const response = await request.json();
+    deleteMsg.remove();
+    msgContainer.innerHTML += ` <div><span class="mr-8 gray">${response.messageCreate}</span> <b>${response.messageAuthor}</b>: ${response.messageText}</div>`;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
