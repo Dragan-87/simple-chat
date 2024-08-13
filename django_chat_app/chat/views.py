eec6703d8508e74a0251d45fdf38a841ed075957
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-08-13 17:13:15 +0200

    chore: Update chat view to include author name in message response

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -3,18 +3,25 @@ from .models import Message, Chat
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.core import serializers
+from django.contrib.auth.models import User
+import json
 
 
 
 @login_required(login_url='/login/')
 def index(request):
-    if request.method == "POST" and request.POST['textmessage'] != '':
+    if request.method == "POST" and request.POST['textmessage'] is not None:
         myChat = Chat.objects.get(id=1)
         new_msg = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        author_name = new_msg.author.username
         serializerd_obj = serializers.serialize('json', [new_msg])
-        return JsonResponse(serializerd_obj[1:-1], safe=False)
-
-
+        
+        msg_data = json.loads(serializerd_obj)
+        msg_data[0]['fields']['author'] = author_name
+        msg_json = json.dumps(msg_data)
+        return JsonResponse(msg_json[1:-1], safe=False)
+    
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
+
 
--- django_chat_app/static/js/script.js
+++ django_chat_app/static/js/script.js
@@ -1,3 +1,6 @@
+allMessages = [];
+
+
 function clearForm() {
   document.getElementById('username').value = '';
   document.getElementById('first_name').value = '';
@@ -32,6 +35,8 @@ function setAllFormData() {
   let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
   form.append('textmessage', chatMsg.value);
   form.append('csrfmiddlewaretoken', token);
+  chatMsg.value = '';
+  token = '';
   return form;
 }
 
@@ -55,8 +60,26 @@ async function sendMessage() {
     const data = await JSON.parse(response);
     deleteMsg.remove();
 
-    msgContainer.innerHTML += ` <div><span class="mr-8 gray">${data['created_at']}</span> <b>${data['author']}</b>: ${data['text']}</div>`;
+    msgContainer.innerHTML += ` <div><span class="mr-8 gray">${new Date(
+      data.fields.created_at
+    ).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    })}</span> <b>${data.fields.author}</b>: ${data.fields.text}</div>`;
   } catch (error) {
     console.error('Error:', error);
   }
+}
+
+
+async function loadMessagesFromServer() {
+    try {
+        const request = await fetch('/chat/');
+        const response = await request.json();
+        const data = await JSON.parse(response);
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
