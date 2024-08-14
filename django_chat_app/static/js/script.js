fd747e0f4fff3f251996b467c6cbbf8859525adb
Dragan Saric <107239220+Dragan-87@users.noreply.github.com>
2024-08-14 14:37:02 +0200

    chore: Update chat view to fix typo in variable name, improve form submission handling, and include author name in message response

--- django_chat_app/static/js/script.js
+++ django_chat_app/static/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Clears the form by resetting the values of the input fields.
+ */
 function clearForm() {
   document.getElementById('username').value = '';
   document.getElementById('first_name').value = '';
@@ -6,6 +9,23 @@ function clearForm() {
   document.getElementById('password').value = '';
 }
 
+/**
+ * Clears the registration form by resetting the values of all input fields.
+ */
+function clearRegisterFrom() {
+  document.getElementById('username').value = '';
+  document.getElementById('first_name').value = '';
+  document.getElementById('last_name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('confirm_password').value = '';
+}
+
+/**
+ * Checks if the password and confirm password fields match, and if all required fields are filled.
+ * @returns {boolean} Returns true if the password and confirm password fields match and all required fields are filled, otherwise returns false.
+ */
 function checkPasswordMatch() {
   registerButton.disabled = true;
   if (
@@ -24,6 +44,11 @@ function checkPasswordMatch() {
   return true;
 }
 
+/**
+ * Checks if the message text is empty.
+ *
+ * @returns {boolean} Returns true if the message text is not empty, otherwise false.
+ */
 function isMessageTextEmpty() {
   if (!chatMsg.value.trim()) {
     document.getElementById('sendButton').disabled = true;
@@ -33,6 +58,10 @@ function isMessageTextEmpty() {
   return true;
 }
 
+/**
+ * Creates a new FormData object and sets the necessary form data.
+ * @returns {FormData} The FormData object with the form data set.
+ */
 function setAllFormData() {
   let form = new FormData();
   let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
@@ -43,6 +72,131 @@ function setAllFormData() {
   return form;
 }
 
+/**
+ * Formats a given date into a string representation.
+ *
+ * @param {Date} date - The date to be formatted.
+ * @returns {string} The formatted date string.
+ */
+function formatDate(date) {
+  const d = new Date(date);
+  const monthName = d.toLocaleString('de-DE', { month: 'short' }).split(' ')[0];
+  const day = d.getDate();
+  const year = d.getFullYear();
+
+  const formattedDate = `${monthName}. ${day}, ${year}`;
+  return formattedDate;
+}
+
+/**
+ * Handles errors during registration.
+ *
+ * @param {Object} data - The error data.
+ */
+function handleRegisterErrors(data) {
+  if (data.fields && data.fields.wrong_password) {
+    messageContainer.innerHTML = '';
+    messageContainer.innerHTML += requestHTMLTemplate(
+      data.fields.wrong_password
+    );
+    setTimeout(() => {
+      messageContainer.innerHTML = '';
+    }, 2000);
+    return;
+  }
+
+  if (data.fields && data.fields.user_exists) {
+    messageContainer.innerHTML = '';
+    messageContainer.innerHTML += requestHTMLTemplate(data.fields.user_exists);
+    setTimeout(() => {
+      messageContainer.innerHTML = '';
+    }, 2000);
+    return;
+  }
+}
+
+/**
+ * Handles the successful registration of a user.
+ *
+ * @param {Object} data - The data returned from the registration request.
+ */
+function handleSuccessfulyRegister(data) {
+  if (data.fields && data.fields.username) {
+    messageContainer.innerHTML = '';
+    messageContainer.innerHTML += requestHTMLTemplate(data.fields.username);
+    setTimeout(() => {
+      window.location.href = '/login/';
+    }, 1000);
+  }
+}
+
+/**
+ * Generates an HTML template with a container div that slides in from the left.
+ *
+ * @param {string} data - The HTML content to be inserted into the template.
+ * @returns {string} The generated HTML template.
+ */
+function requestHTMLTemplate(data) {
+  return `<div class="container slideInFromLeft">${data}</div>`;
+}
+
+/**
+ * Creates a FormData object with the values from the registration form.
+ * @returns {FormData} The FormData object containing the form data.
+ */
+function setRegisterFormData() {
+  let form = new FormData();
+  form.append('username', document.getElementById('username').value.trim());
+  form.append('first_name', document.getElementById('first_name').value.trim());
+  form.append('last_name', document.getElementById('last_name').value.trim());
+  form.append('email', document.getElementById('email').value.trim());
+  form.append('password', document.getElementById('password').value.trim());
+  form.append(
+    'confirm_password',
+    document.getElementById('confirm_password').value.trim()
+  );
+  let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+  form.append('csrfmiddlewaretoken', token);
+  return form;
+}
+
+/**
+ * Performs a logout operation.
+ * @async
+ * @function logout
+ * @returns {Promise<void>} - A promise that resolves when the logout operation is completed.
+ */
+async function logout() {
+  let form = new FormData();
+  let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+  form.append('csrfmiddlewaretoken', token);
+
+  let requestOptions = {
+    method: 'POST',
+    body: form,
+  };
+
+  try {
+    const response = await fetch('/logout/', requestOptions);
+    const data = await response.json();
+
+    if (data.fields.logout) {
+      window.location.href = '/login/';
+    } else {
+      console.error('Logout failed');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+/**
+ * Sends a message to the server and updates the message container with the response.
+ * @async
+ * @function sendMessage
+ * @returns {Promise<void>} A promise that resolves when the message is sent and the message container is updated.
+ * @throws {Error} If there is an error during the message sending process.
+ */
 async function sendMessage() {
   let form = setAllFormData();
   let requestOptions = {
@@ -72,21 +226,13 @@ async function sendMessage() {
 }
 
 /**
- * Formats a given date into a string representation.
+ * Performs a login request using the provided username and password.
  *
- * @param {Date} date - The date to be formatted.
- * @returns {string} The formatted date string.
+ * @async
+ * @function login
+ * @returns {Promise<void>} A promise that resolves when the login request is completed.
+ * @throws {Error} If an error occurs during the login request.
  */
-function formatDate(date) {
-  const d = new Date(date);
-  const monthName = d.toLocaleString('de-DE', { month: 'short' }).split(' ')[0];
-  const day = d.getDate();
-  const year = d.getFullYear();
-
-  const formattedDate = `${monthName}. ${day}, ${year}`;
-  return formattedDate;
-}
-
 async function login() {
   let form = new FormData();
   let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
@@ -123,6 +269,7 @@ async function login() {
     console.error('Error:', error);
   }
 }
+
 /**
  * Registers a user by sending a POST request to the '/register/' endpoint.
  *
@@ -147,91 +294,5 @@ async function register() {
     handleRegisterErrors(data);
   } catch (e) {
     console.log(e);
-  }
-}
-
-function handleRegisterErrors(data) {
-  if (data.fields && data.fields.wrong_password) {
-    messageContainer.innerHTML = '';
-    messageContainer.innerHTML += requestHTMLTemplate(
-      data.fields.wrong_password
-    );
-    setTimeout(() => {
-      messageContainer.innerHTML = '';
-    }, 2000);
-    return;
-  }
-
-  if (data.fields && data.fields.user_exists) {
-    messageContainer.innerHTML = '';
-    messageContainer.innerHTML += requestHTMLTemplate(data.fields.user_exists);
-    setTimeout(() => {
-      messageContainer.innerHTML = '';
-    }, 2000);
-    return;
-  }
-}
-
-function handleSuccessfulyRegister(data) {
-  if (data.fields && data.fields.username) {
-    messageContainer.innerHTML = '';
-    messageContainer.innerHTML += requestHTMLTemplate(data.fields.username);
-    setTimeout(() => {
-      window.location.href = '/login/';
-    }, 1000);
-  }
-}
-
-function requestHTMLTemplate(data) {
-  return `<div class="container slideInFromLeft">${data}</div>`;
-}
-
-function setRegisterFormData() {
-  let form = new FormData();
-  form.append('username', document.getElementById('username').value.trim());
-  form.append('first_name', document.getElementById('first_name').value.trim());
-  form.append('last_name', document.getElementById('last_name').value.trim());
-  form.append('email', document.getElementById('email').value.trim());
-  form.append('password', document.getElementById('password').value.trim());
-  form.append(
-    'confirm_password',
-    document.getElementById('confirm_password').value.trim()
-  );
-  let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-  form.append('csrfmiddlewaretoken', token);
-  return form;
-}
-
-function clearRegisterFrom() {
-  document.getElementById('username').value = '';
-  document.getElementById('first_name').value = '';
-  document.getElementById('last_name').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('password').value = '';
-  document.getElementById('password').value = '';
-  document.getElementById('confirm_password').value = '';
-}
-
-async function logout() {
-  let form = new FormData();
-  let token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-  form.append('csrfmiddlewaretoken', token);
-
-  let requestOptions = {
-    method: 'POST',
-    body: form,
-  };
-
-  try {
-    const response = await fetch('/logout/', requestOptions);
-    const data = await response.json();
-
-    if (data.fields.logout) {
-      window.location.href = '/login/';
-    } else {
-      console.error('Logout failed');
-    }
-  } catch (error) {
-    console.error('Error:', error);
   }
 }
