f84e0c17b95d9002e44cee1ac3edb6df229003f0
Illutas <107239220+Illutas@users.noreply.github.com>
2024-08-15 16:53:33 +0200

    chore: Refactor login and logout functionality, improve form submission handling, and fix authentication issues

--- django_chat_app/authService/tests.py
+++ django_chat_app/authService/tests.py
@@ -4,18 +4,21 @@ import json
 
 
 class TestLogout(TestCase):
-    def test_login_success(self):
+    def setUp(self):
+        self.client = Client()
         self.user = User.objects.create_user(username='test_user', password='asdasd')
-        response = self.client.post('/login/', data={'username': self.user.username, 'password': self.user.password})
-        assert response.status_code == 200
-        assert self.user.is_authenticated
+
+    def test_login_success(self):
+        response = self.client.post('/login/', data={'username': self.user.username, 'password': 'asdasd'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self.client.login(username='test_user', password='asdasd'))
 
     def test_login_failed(self):
-        self.user = User.objects.create_user(username='test_user', password='asdasd')
-        response = self.client.post('/login/', data={'username': 'username_flase', 'password': 'password_false'})
+        response = self.client.post('/login/', data={'username': 'username_false', 'password': 'password_false'})
         self.assertEqual(response.status_code, 200)
         response_json = json.loads(response.content.decode())
         self.assertIn('fields', response_json)
         fields = response_json.get('fields', {})
         self.assertIn('wrong_password', fields)
         self.assertEqual(fields['wrong_password'], 'Benutzername oder Passwort falsch')
+        self.assertFalse(self.client.login(username='username_false', password='password_false'))
